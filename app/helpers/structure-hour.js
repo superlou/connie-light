7ca592f44855bd183b30c9c995100816476ae499
superlou <lousimons@gmail.com>
2015-10-11 20:23:42 -0400

    Switch to new helper format

--- app/helpers/structure-hour.js
+++ app/helpers/structure-hour.js
@@ -1,10 +1,8 @@
 import Ember from 'ember';
 
-export function structureHour(params/*, hash*/) {
+export default Ember.Helper.helper(function(params) {
   var period = params[0].format('{12hr}{tt}');
   period = period.replace('pm', '<span class="period pm">pm</span>');
   period = period.replace('am', '<span class="period am">am</span>');
   return period.htmlSafe();
-}
-
-export default Ember.HTMLBars.makeBoundHelper(structureHour);
+});
