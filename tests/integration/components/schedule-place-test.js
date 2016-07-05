3f41d7931765b9437d1a30efefdfd911d0d1d579
superlou <lousimons@gmail.com>
2016-07-05 19:13:34 -0400

    Added basic test for schedule-place component

--- tests/integration/components/schedule-place-test.js
+++ tests/integration/components/schedule-place-test.js
@@ -6,20 +6,13 @@ moduleForComponent('schedule-place', 'Integration | Component | schedule place',
 });
 
 test('it renders', function(assert) {
-  
+  assert.expect(1);
+
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
 
-  this.render(hbs`{{schedule-place}}`);
+  this.set('place', {name: 'Cool Place'});
+  this.render(hbs`{{schedule-place place=place}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:" + EOL +
-  this.render(hbs`
-    {{#schedule-place}}
-      template block text
-    {{/schedule-place}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), 'Cool Place');
 });
