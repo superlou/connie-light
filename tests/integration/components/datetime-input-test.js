c4bbf28ccc005efad7bff19a8340893ddba1d61e
superlou <lousimons@gmail.com>
2016-07-05 18:44:45 -0400

    Added very basic datetime-input test

--- tests/integration/components/datetime-input-test.js
+++ tests/integration/components/datetime-input-test.js
@@ -6,21 +6,14 @@ moduleForComponent('datetime-input', 'Integration | Component | datetime input',
 });
 
 test('it renders', function(assert) {
-  assert.expect(2);
+  assert.expect(1);
 
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{datetime-input}}`);
+  this.set('date', new Date("2015-10-05T10:00:00-04:00"));
+  this.render(hbs`{{datetime-input value=date}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#datetime-input}}
-      template block text
-    {{/datetime-input}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  // todo This is brittle and may fail if run in a timezone other than EST
+  assert.equal(this.$('input').val(), '10:00 am, Oct 5th 2015');
 });
